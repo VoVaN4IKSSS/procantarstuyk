00ea9612508ef472270d51d2ccc551d0c3e43a82
VoVaN4IKSSS <tsapenko2013vova@gmail.com>
2025-07-06 12:18:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 
-        const toggleBtn = document.getElementById('menu-toggle');
-        const page = document.getElementById('page');
-        const hoverSound = document.getElementById('hover-sound');
-        const links = document.querySelectorAll('nav a');
+const toggleBtn = document.getElementById('menu-toggle');
+const page = document.getElementById('page');
+const hoverSound = document.getElementById('hover-sound');
+const links = document.querySelectorAll('nav a');
 
-        let soundEnabled = false;
+let soundEnabled = false;
 
-        function enableSound() {
-            soundEnabled = true;
-            document.removeEventListener('click', enableSound);
-        }
+function enableSound() {
+    soundEnabled = true;
+    document.removeEventListener('click', enableSound);
+}
 document.addEventListener('click', enableSound);
 
-        toggleBtn.addEventListener('click', () => {
-            page.classList.toggle('collapsed');
-        });
+toggleBtn.addEventListener('click', () => {
+    page.classList.toggle('collapsed');
+});
 
-        links.forEach(link => {
-            link.addEventListener('mouseenter', () => {
-                if (soundEnabled) {
-                    hoverSound.currentTime = 0;
-                    hoverSound.play().catch(() => { }); // на всяк випадок не зламає
-                }
-            });
-        });
+links.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        if (soundEnabled) {
+            hoverSound.currentTime = 0;
+            hoverSound.play().catch(() => { }); // на всяк випадок не зламає
+        }
+    });
+});
+const goose = document.getElementById('goose');
+const sound = document.getElementById('gooseSound');
+
+goose.addEventListener('click', () => {
+    sound.currentTime = 0;
+    sound.play();
+});
